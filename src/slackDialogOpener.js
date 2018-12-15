327d23bc1c3aeee73cbc6ed79a6c1cc2fef6d5ed
Tomasz Netczuk <contact@netczuk.pl>
2018-12-15 16:49:37 +0100

    Open burrito dialog using slack api

--- src/slackDialogOpener.js
+++ src/slackDialogOpener.js
@@ -1,5 +1,32 @@
 // @flow strict
 
+import got from "got";
 import type { Dialog } from "./types";
 
-export default function slackDialogOpener(triggerId: string, dialog: Dialog) {}
+export const url = "https://slack.com/api/dialog.open";
+
+export default async function slackDialogOpener(
+  triggerId: string,
+  dialog: Dialog,
+) {
+  return got.post(url, {
+    json: true,
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN ||
+        ""}`,
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: buildBody(triggerId, dialog),
+  });
+}
+
+function buildBody(triggerId: string, dialog: Dialog) {
+  return {
+    trigger_id: triggerId,
+    dialog: {
+      ...dialog,
+      callback_id: dialog.callbackId,
+      submit_label: dialog.submitLabel,
+    },
+  };
+}
